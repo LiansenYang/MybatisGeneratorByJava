8fdfb2a07f22a9d524be5287fc2c137d34596d3d
LiansenYang <1558145743@qq.com>
2020-04-06 01:53:35 +0800

    功能可用，但是没有加上mybatis分页功能

--- src/main/java/com/yangLs/MybatisGeneratorByJava/App.java
+++ src/main/java/com/yangLs/MybatisGeneratorByJava/App.java
@@ -1,13 +1,31 @@
 package com.yangLs.MybatisGeneratorByJava;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, Exception
     {
-        System.out.println( "Hello World!" );
+    	List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        String path = App.class.getClassLoader().getResource("generatorConfig.xml").getPath();
+        File configFile = new File(path);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
     }
 }
